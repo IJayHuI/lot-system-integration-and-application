0120749534f7fd512f27defc74b10c22274afee2
IJayHuI <2949763264@qq.com>
2025-11-13 03:15:02 +0800

    预创建首页

---
+++ src/components/LsiaaMissionInfo.vue
@@ -0,0 +1,1 @@
+<template>mission-info</template>
--- src/views/mains/LsiaaMain.vue
+++ src/views/mains/LsiaaMain.vue
@@ -1,2 +1,26 @@
-<script setup></script>
-<template></template>
+<script setup>
+  import { useThemeVars } from 'naive-ui'
+  import { mainData } from '@/storages/LsiaaViewData'
+
+  import LsiaaMenu from '@/components/LsiaaMenu.vue'
+  import LsiaaGroupPerformance from '@/components/LsiaaGroupPerformance.vue'
+  import LsiaaMissionInfo from '@/components/LsiaaMissionInfo.vue'
+  const themeVars = useThemeVars()
+</script>
+<template>
+  <div :style="{ '--bodyColor': themeVars.bodyColor }" class="relative min-h-screen bg-(--bodyColor) z-10">
+    <n-layout position="absolute">
+      <n-layout has-sider position="absolute">
+        <n-layout-sider><lsiaa-menu /></n-layout-sider>
+        <n-layout-content v-if="mainData.projectInfo">
+          <n-layout-header class="h-16 flex items-center text-2xl pl-2 font-bold">{{ mainData.projectInfo.fullName }}</n-layout-header>
+          <n-tabs animated justify-content="space-evenly">
+            <n-tab-pane name="group-performance" tab="小组完成情况"><lsiaa-group-performance /></n-tab-pane>
+            <n-tab-pane name="mission-info" tab="任务详情"><lsiaa-mission-info /></n-tab-pane>
+          </n-tabs>
+        </n-layout-content>
+        <n-layout-content v-else content-class="flex items-center justify-center text-3xl pl-2 font-bold">请选择一个项目</n-layout-content>
+      </n-layout>
+    </n-layout>
+  </div>
+</template>
