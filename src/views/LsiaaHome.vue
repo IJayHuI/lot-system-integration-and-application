5b029967419c8be9ce9a461d8993a680d05abae5
IJayHuI <2949763264@qq.com>
2025-11-13 02:42:44 +0800

    顶部首页完善

--- src/views/LsiaaHome.vue
+++ src/views/LsiaaHome.vue
@@ -1,2 +1,13 @@
-<script setup></script>
-<template></template>
+<script setup>
+  const appVersion = __APP_VERSION__
+</script>
+<template>
+  <n-space vertical justify="center" align="center" class="h-40 sticky top-0 z-0">
+    <n-space justify="center" align="center">
+      <img src="/vite.svg" alt="logo" class="w-12 h-12" />
+      <span class="text-3xl font-bold">物联网系统集成与应用</span>
+    </n-space>
+    <span class="text-xl">智评教系统</span>
+    <span>系统版本：{{ appVersion }}</span>
+  </n-space>
+</template>
--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import pkg from './package.json'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  define: {
+    __APP_VERSION__: JSON.stringify(pkg.version)
+  },
   server: {
     host: '0.0.0.0'
   },
