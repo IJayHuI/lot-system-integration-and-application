6111e423f06c8dad0fc96e44474b85fbb1829e91
IJayHuI <2949763264@qq.com>
2025-11-13 17:54:37 +0800

    样式更改

--- src/views/mains/LsiaaMain.vue
+++ src/views/mains/LsiaaMain.vue
@@ -14,12 +14,12 @@
         <n-layout-sider><lsiaa-menu /></n-layout-sider>
         <n-layout-content v-if="mainData.projectInfo">
           <n-layout-header class="h-16 flex items-center text-2xl pl-2 font-bold">{{ mainData.projectInfo.fullName }}</n-layout-header>
-          <n-tabs animated justify-content="space-evenly">
+          <n-tabs pane-class="flex flex-col gap-2 !pl-2 !pr-2" animated justify-content="space-evenly">
             <n-tab-pane name="group-performance" tab="小组完成情况"><lsiaa-group-performance /></n-tab-pane>
             <n-tab-pane name="mission-info" tab="任务详情"><lsiaa-mission-info /></n-tab-pane>
           </n-tabs>
         </n-layout-content>
-        <n-layout-content v-else content-class="flex items-center justify-center text-3xl pl-2 font-bold">请选择一个项目</n-layout-content>
+        <n-layout-content v-else content-class="flex items-center justify-center text-3xl font-bold">请选择一个项目</n-layout-content>
       </n-layout>
     </n-layout>
   </div>
